e73d8f4ba0db13e130cde6d2eb2e401cc43ef6b6
tkdeng <web@tkdeng.com>
2026-03-14 16:13:53 -0400

    updated

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -75,5 +75,11 @@ func Compile(path string, vars map[string]string) error {
 	// #layout pages should be copied over
 	// also, embed const vars when possible, otherwise keep placeholder for future vars
 
+	//todo: add separate method for precompiled vars (and runtime var methods)
+	// similar to what webx module does with {lorem}
+	// may make it easier for admins to expand on these
+	// may also add future extensions using {:plugin key=value} method
+	// and use {:plugin key=value { content }} for simplicity of multiline content
+
 	return nil
 }
